7948cb5a191f542eac041188046d591f563fcff8
gerogz <67670577+gerogz@users.noreply.github.com>
2023-07-24 07:27:58 -0400

    Delete IdeaProjects directory

--- IdeaProjects/phase4/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.signupmysql {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.sql;
-
-
-    opens com.example.signupmysql to javafx.fxml;
-    exports com.example.signupmysql;
-}
