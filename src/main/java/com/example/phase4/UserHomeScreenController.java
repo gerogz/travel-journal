77d5e1b9e319b6f7ea1e844617e4f9126c511c72
Andy Yu <ayu303@gatech.edu>
2023-07-25 22:56:06 +0200

    Add ViewTrips scene builder, update buttons

--- src/main/java/com/example/phase4/UserHomeScreenController.java
+++ src/main/java/com/example/phase4/UserHomeScreenController.java
@@ -7,7 +7,6 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
-
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -33,7 +32,6 @@ public class UserHomeScreenController implements Initializable {
 
     @FXML
     private Button button_createjournalentry;
-
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
@@ -61,7 +59,7 @@ public class UserHomeScreenController implements Initializable {
         button_viewtrips.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
-                DBUtils.changeScene(event, "user-settings.fxml", "log in", null);
+                DBUtils.changeScene(event, "view-trips.fxml", "log in", null);
             }
         });
 
@@ -85,4 +83,3 @@ public class UserHomeScreenController implements Initializable {
         label_userhomescreen.setText(username);
     }
 }
-
